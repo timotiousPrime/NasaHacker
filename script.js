c2ff15edfda0e8a9daa1e24f433cc1d3cbcad7be
timotiousPrime <timodb031@gmail.com>
2021-04-10 20:44:42 +0200

    add matrix background and css file

--- script.js
+++ script.js
@@ -115,4 +115,57 @@ let btn = document.getElementsByClassName('btn')
 
 //Link to Space station <iframe width="480" height="270" src="https://ustream.tv/embed/17074538" scrolling="no" allowfullscreen webkitallowfullscreen frameborder="0" style="border: 0 none transparent;"></iframe>
 
+//Matrix background
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+
+//make canvas full screen
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+//Characters to display
+text = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
+// Convert the string to an array of single characters
+text = text.split('');
+
+let font_size = 10;
+//Number of columns for the rain
+let columns = canvas.width/font_size; 
+
+//An array of drops - one per column
+let drops = [];
+
+//x below os the x coordinate
+//1 = y coordinate of the drops(same for every drop initially)
+for(let x = 0; x < columns; x++)
+    drops[x] = 1;
+
+//Drawing the characters
+function draw(){
+    //Black BG for the canvas
+    //translucent BG to show trail
+    ctx.fillStyle = 'rgba(0,0,0,0.04)';
+    ctx.fillRect(0,0, canvas.width, canvas.height)
+
+    ctx.fillStyle = '#0f6';
+    ctx.font = font_size + 'px arial';
+    //Looping over drops
+    for(let i=0; i<drops.length; i++){
+        //a random character to print
+        let matrixDrop = text[Math.floor(Math.random()*text.length)];
+        //x = i*font_size, y = value of drops[i]*font_size
+        ctx.fillText(matrixDrop, i*font_size, drops[i]*font_size);
+
+        //sending the drops back to the top randomly after it as crossd the screen
+        //adding randomness to the reset to make the drops scattered on the y axis
+        if(drops[i]*font_size > canvas.height && Math.random() > 0.975)
+        drops[i] = 0;
+
+        //incrementing y coordinate
+        drops[i]++
+    }
+}
+
+setInterval(draw, 33);
 
